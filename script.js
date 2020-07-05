894b103b541515e735b7655472530a5ee076407c
vronney <ronneyvargas@gmail.com>
2020-07-05 12:35:07 -0500

    Added mobile view and clear function

--- script.js
+++ script.js
@@ -40,11 +40,77 @@ async function fetchData() {
 
     });
 
-    let cardBody = document.querySelector('.container');
-    
-    cardBody.innerHTML = html;
-    
-    console.log(data);
+  let cardBody = document.querySelector('.container');
+  cardBody.innerHTML = html;
+
+  console.log(data);
+
+  filterData();
 }
+
+// Filter skill tags
+const filterData = () => {
+  const tagsToFilterBy = [];
+  let tags = '';
+  let tagContent = '';
+  let filterContent = '';
+  
+  Array.from(document.querySelectorAll('.skill')).forEach(tagEl => {
+    if (!tagsToFilterBy.includes(tagEl.textContent)) {
+      tagsToFilterBy.push(tagEl.textContent);
+    }
+    return tagsToFilterBy;
+  });
+
+
+  for (let i = 0; i < tagsToFilterBy.length; i++) {
+    tags = tagsToFilterBy[i];
+    filterContent = filterContent + `
+    <li><span class="tag">${tagsToFilterBy[i]}</span></li>
+    `;
+  }
+
+  let clearTag = document.createElement('p');
+  clearTag.classList.add('clear');
+  clearTag.innerHTML = 'Reset';
+
+  document.querySelector('.search-container').appendChild(clearTag);
+    
+  let searchBody = document.querySelector('.search-container .tags');
+  
+  searchBody.innerHTML = filterContent;
+
+  removeSkill();
+  resetFilter();
+
+}
+
+// Remove skill from filter and remove job from main display
+
+const removeSkill = () => {
+  let clickedJob = '';
+  let clickedTag = document.querySelectorAll('.tag').forEach(clickedTag => {
+
+    clickedTag.addEventListener('click', e => {
+      let jobCards = document.querySelectorAll('.skill').forEach(clickedJob => {
+        if (clickedJob.textContent === e.target.textContent) {
+          clickedJob.parentNode.parentNode.parentNode.remove();
+        }        
+      });
+      e.target.style.display = 'none';
+    });
+  });
+
+}
+
+// Filter reset
+
+const resetFilter = () => {
+  let clearTag = document.querySelector('.clear');
+  clearTag.addEventListener('click', () => {
+    fetchData();
+  });
+}
+
 
 fetchData();
